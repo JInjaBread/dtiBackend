2aa5df1506e27844304e8cb88db7bcb64fcc8b6e
JInjaBread <82684344+JInjaBread@users.noreply.github.com>
2022-08-01 08:40:46 +0800

    Update settings.py

--- dtiBackend/settings.py
+++ dtiBackend/settings.py
@@ -10,14 +10,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t0smx*0#^vyvc=u97#$oux3)(g$w)(#fmb@xqi&s%xw&i(&h^f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 
 CORS_ORIGIN_ALLOW_ALL = True
-ALLOWED_HOSTS = ['dtiapp.herokuapp.com/', '192.168.1.181']
+ALLOWED_HOSTS = [*]
 
 # Application definition
 
